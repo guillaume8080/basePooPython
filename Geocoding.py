38fe6cff0be1597f380148ae4514c357e80627ed
Guillaume Gouy <guillaumegouy@sfr.fr>
2021-11-07 11:22:24 +0100

    commit

---
+++ Geocoding.py
@@ -0,0 +1,6 @@
+class Geocoding(object):
+    def __init__(self):
+        pass# necessaire pour les metodes sans contenu
+
+    def unPrint(self):
+        print("J ai appelé ma première méthode de classe")
---
+++ Main.py
@@ -0,0 +1,12 @@
+from Geocoding import Geocoding
+
+class Main(object):
+    def __init__(self):
+        pass
+
+    def Appel(self):
+        obj = Geocoding()
+        obj.unPrint()
+
+obj = Main()
+obj.Appel()
